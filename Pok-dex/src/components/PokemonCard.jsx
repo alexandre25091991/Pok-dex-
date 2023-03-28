cd4c8f5392d1bc91062f2c0c5aeae97a292b448a
alexandre ROUZIERE <alexandre.rouziere1@gmail.com>
2023-03-28 17:06:18 +0200

    exercice 3

--- Pok-dex/src/components/PokemonCard.jsx
+++ Pok-dex/src/components/PokemonCard.jsx
@@ -1,12 +1,24 @@
 
 
 function PokemonCard() {
+    const pokemon=pokemonList[1];
+    console.log(pokemon);
     return (
-        <figure>
-            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="pokemon"></img>
-            <figcaption>bulbasaur vyrrhgtrgtrfgi</figcaption>
-        </figure >
-    )
+        
+        pokemon.imgSrc ? <figure><img src={pokemon.imgSrc}/><figcaption>bulbasaur</figcaption></figure> : <p>{pokemon.name}</p>
+        
+    );
 }
+
+const pokemonList = [
+    {
+      name: "bulbasaur",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "mew",
+    },
+  ];
 
 export default PokemonCard;
