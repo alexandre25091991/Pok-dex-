e8fa98b8ce4c87b84e5f80705637a2ccbffb9007
alexandre ROUZIERE <alexandre.rouziere1@gmail.com>
2023-03-29 12:02:43 +0200

    exercice props

--- Pok-dex/src/App.jsx
+++ Pok-dex/src/App.jsx
@@ -2,14 +2,21 @@
 import './App.css'
 import PokemonCard from './components/PokemonCard'
 
+
 function App() {
-
-
   return (
-    <div className="App">
-      <PokemonCard/>
+    <div>
+      <PokemonCard  props={pokemonList[0]}/>
     </div>
-  );
-}
-
+  )};
+const pokemonList = [
+  {
+    name: "bulbasaur",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "mew",
+  },
+];
 export default App
--- Pok-dex/src/components/PokemonCard.jsx
+++ Pok-dex/src/components/PokemonCard.jsx
@@ -1,24 +1,12 @@
 
 
-function PokemonCard() {
-    const pokemon=pokemonList[1];
-    console.log(pokemon);
+function PokemonCard({props}) {
+    const pokemon=props;
     return (
-        
         pokemon.imgSrc ? <figure><img src={pokemon.imgSrc}/><figcaption>bulbasaur</figcaption></figure> : <p>{pokemon.name}</p>
-        
     );
 }
 
-const pokemonList = [
-    {
-      name: "bulbasaur",
-      imgSrc:
-        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-    },
-    {
-      name: "mew",
-    },
-  ];
+
 
 export default PokemonCard;
