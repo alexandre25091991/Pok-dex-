a09344ba407301a871e09c38631bab775ff66b45
alexandre ROUZIERE <alexandre.rouziere1@gmail.com>
2023-03-29 15:10:37 +0200

    propTypes

--- Pok-dex/src/App.jsx
+++ Pok-dex/src/App.jsx
@@ -1,7 +1,7 @@
 
 import './App.css'
 import PokemonCard from './components/PokemonCard'
-
+import PropTypes from "prop-types";
 
 function App() {
   return (
--- Pok-dex/src/components/PokemonCard.jsx
+++ Pok-dex/src/components/PokemonCard.jsx
@@ -1,4 +1,4 @@
-
+import PropTypes from "prop-types";
 
 function PokemonCard({props}) {
     const pokemon=props;
@@ -6,7 +6,11 @@ function PokemonCard({props}) {
         pokemon.imgSrc ? <figure><img src={pokemon.imgSrc}/><figcaption>bulbasaur</figcaption></figure> : <p>{pokemon.name}</p>
     );
 }
-
-
+PokemonCard.propTypes = {
+  pokemon: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    imgSrc: PropTypes.img,
+  })
+}
 
 export default PokemonCard;
